a9d89915c3e33a6074c2445ee0768e95c3ce7a58
JocelynWeb <84760377+JocelynWeb@users.noreply.github.com>
2021-05-25 12:17:58 -0500

    Update CtrlSaludo.js

--- js/CtrlSaludo.js
+++ js/CtrlSaludo.js
@@ -1,10 +1,18 @@
-export class CtrlSaludo {
-  /**@param {string} dato */
-  calculaSaludo(dato){
-    const nombreSinEspacios= dato ? dato.trim(): "";
-    if (!nombreSinEspacios) {
-      throw new Error("Falta proporcionar el nombre");
-    }
-    return '${nombreSinEspacios}';
-  }
-}
+      const ctrlSaludo= new CtrlSaludo();
+      formSaludo.addEventListener("submit", evt => {
+        try {
+          evt.preventDefault();
+          const datos=new FormData(formSaludo);
+          const bol=datos.get("boleta");
+          const nom=datos.get("nombre");
+          const gru=datos.get("grupo");
+          const mat=datos.get("materia");
+          const fec=datos.get("fecha");
+          var dato= "Boleta: "+bol +" Nombre: "+nom +" Grupo: "+gru +" Materia: "+mat +" Fecha: "+fec ;
+          const saludo= ctrlSaludo.calculaSaludo(dato);
+          formSaludo.salida.value= saludo;
+        } catch (error) {
+          console.log(error);
+          formSaludo.salida.value=error.message;
+        }
+      });
