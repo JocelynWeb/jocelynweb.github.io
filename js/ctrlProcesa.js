bcdc3df974b1658af376869071ce98b608d05886
JocelynWeb <84760377+JocelynWeb@users.noreply.github.com>
2021-05-25 13:50:13 -0500

    Update ctrlProcesa.js

--- js/ctrlProcesa.js
+++ js/ctrlProcesa.js
@@ -1,20 +1,20 @@
-"uso estricto" ;
-var  forma  =  documento . getElementById ( "forma" ) ,
-    salidaBoleta  =  documento . getElementById ( "salidaBoleta" ) ,
-    salidaNombre  =  documento . getElementById ( "salidaNombre" ) ,
-    salidaGrupo  =  documento . getElementById ( "salidaGrupo" ) ,
-    salidaMateria  =  documento . getElementById ( "salidaMateria" ) ,
-    txtFecha  =  forma [ "fecha" ] , 
-    salidaFecha  =  documento . getElementById ( "salidaFecha" ) ,
-    salidaDatos  =  documento . getElementById ( "salidaDatos" ) ;
+"use strict";
+var forma= document.getElementById("forma"),
+    salidaBoleta= document.getElementById("salidaBoleta"),
+    salidaNombre= document.getElementById("salidaNombre"),
+    salidaGrupo= document.getElementById("salidaGrupo"),
+    salidaMateria= document.getElementById("salidaMateria"),
+    txtFecha= forma["fecha"], 
+    salidaFecha= document.getElementById("salidaFecha"),
+    salidaDatos= document.getElementById("salidaDatos");
 
-function  captura ( )  {
+function  captura() {
     
-    var  fecha  =  nueva  fecha ( valor txtFecha . ) ,
-    boleta  =  forma [ "boleta" ] . valor . recortar ( ) ,
-    nombre  =  forma [ "nombre" ] . valor . recortar ( ) ,
-    grupo  =  forma [ "grupo" ] . valor . recortar ( ) ,
-    materia  =  forma [ "materia" ] . valor . recortar ( ) ;
+    var fecha= new Date(txtFecha.value) ,
+    boleta =  forma [ "boleta" ].value.trim(),
+    nombre =  forma [ "nombre" ].value.trim(),
+    grupo =  forma [ "grupo" ].value.trim(),
+    materia =  forma [ "materia" ].value.trim();
 
     salidaDatos . textContent  =  "Boleta:"  +  boleta  +  "|"  +  "Nombre"  +  nombre  +  "|"  +  "Grupo:"  +  grupo 
     +  "|"  +  "Materia:"  +  materia  +  "|"  +  "Fecha:"  +  fecha . toUTCString ( ) ;
