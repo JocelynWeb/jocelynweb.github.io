e9317dee4ac7f1036de19cf5dfe577e8d123ed39
JocelynWeb <84760377+JocelynWeb@users.noreply.github.com>
2021-05-25 12:46:32 -0500

    Delete cmp directory

--- cmp/mi-footer.js
+++
@@ -1,7 +0,0 @@
-class MiFooter extends HTMLElement{
-  connectedCallback(){
-    this.innerHTML=/* html */
-      'Copyright &copy; Garduño Rebollar Lizeth Jocelyn ';
-  }
-}
-customElements.define("mi-footer",MiFooter);
